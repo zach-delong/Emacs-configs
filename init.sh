1facce32ac571b596f38fe33c435d3900954feed
Zachery DeLong <zacherypd@gmail.com>
2015-06-10 13:05:39 -0400

    adding new dotfiles.

--- init.sh
+++ init.sh
@@ -6,6 +6,14 @@ PATH_HOME="/Users/$USER/"
 
 rm -rf $PATH_HOME.vim
 rm -rf $PATH_HOME.vimrc
+rm -rf $PATH_HOME.bash_profile
+rm -rf $PATH_HOME.git-completion.bash
+rm -rf $PATH_HOME.git-prompt.sh
+rm -rf $PATH_HOME.bashrc
 
 ln -s "$PWD/vim" "$PATH_HOME.vim"
 ln -s "$PWD/vimrc" "$PATH_HOME.vimrc"
+ln -s "$PWD/bash_profile" "$PATH_HOME.bash_profile"
+ln -s "$PWD/git-completion.bash" "$PATH_HOME.git-completion.bash"
+ln -s "$PWD/git-prompt.sh" "$PATH_HOME.git-prompt.sh"
+ln -s "$PWD/bashrc" "$PATH_HOME.bashrc"
