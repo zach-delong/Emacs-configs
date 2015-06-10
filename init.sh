ad039f9793beef83b921be790e1ee10638fbffd4
Zachery DeLong <zacherypd@gmail.com>
2015-06-10 13:01:27 -0400

    Fixing symlink issue.

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ TRUE='true'
 USERINPUT=$1
 PATH_HOME="/Users/$USER/"
 
-rm -rf $PATH_HOME.vim/
+rm -rf $PATH_HOME.vim
 rm -rf $PATH_HOME.vimrc
 
-ln -s "$PWD/.vim" "$PATH_HOME.vim"
-ln -s "$PWD/.vimrc" "$PATH_HOME.vimrc"
+ln -s "$PWD/vim" "$PATH_HOME.vim"
+ln -s "$PWD/vimrc" "$PATH_HOME.vimrc"
