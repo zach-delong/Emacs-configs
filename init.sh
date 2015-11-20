5003435935da1b54537a46ccac801b814682538c
Zachery DeLong <zachery.delong@edelman.com>
2015-11-20 10:19:49 -0500

    Updating zshrc configs.

--- init.sh
+++ init.sh
@@ -11,6 +11,7 @@ rm -rf $PATH_HOME.git-completion.bash
 rm -rf $PATH_HOME.git-prompt.sh
 rm -rf $PATH_HOME.bashrc
 rm -rf $PATH_HOME.tmux.conf
+rm -rf $PATH_HOME.zshrc.conf
 
 ln -s "$PWD/vim" "$PATH_HOME.vim"
 ln -s "$PWD/vimrc" "$PATH_HOME.vimrc"
@@ -19,3 +20,4 @@ ln -s "$PWD/git-completion.bash" "$PATH_HOME.git-completion.bash"
 ln -s "$PWD/git-prompt.sh" "$PATH_HOME.git-prompt.sh"
 ln -s "$PWD/bashrc" "$PATH_HOME.bashrc"
 ln -s "$PWD/tmux.conf" "$PATH_HOME.tmux.conf"
+ln -s "$PWD/tmux.conf" "$PATH_HOME.zshrc.conf"
