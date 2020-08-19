a508628e057d8fe395d9c00121615418f14e335b
zach delong <zach.delong@abrigo.com>
2020-08-19 11:09:59 -0400

    Commenting windows files

--- windows.el
+++ windows.el
@@ -1,8 +1,11 @@
+;; The org-directory on windows.
+;; This will be used to identify where org files are in my personal org layer
 (setq org-directory "~/../../Dropbox/org/")
+
+;; The org-brain directory on windows.
+;; This will be used relative to the org-directory
 (setq org-brain-path (concat org-directory "brain/"))
 
 ;; fixes a bug in Windows https://github.com/bbatsov/projectile/issues/1302
-
 ;; This will fix the project indexing method on this computer.
-;; Might want to move this to the windows module later?
 (setq projectile-indexing-method 'alien)
