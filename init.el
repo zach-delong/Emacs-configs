60dd25dca4633765f951f7f712130c8486f1be8e
zach delong <zach.delong@abrigo.com>
2020-10-13 11:45:19 -0400

    Setting line numbers to absolute

--- init.el
+++ init.el
@@ -373,7 +373,7 @@ It should only modify the values of Spacemacs settings."
    ;;   :size-limit-kb 1000)
    ;; When used in a plist, `visual' takes precedence over `relative'.
    ;; (default nil)
-   dotspacemacs-line-numbers 'relative
+   dotspacemacs-line-numbers t
 
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
