cb37b9fe6796605b7eaf75af74e1e58b6319b96d
zach delong <zach.delong@abrigo.com>
2020-10-13 11:43:41 -0400

    Checking in autocompletion tweak
    Snippets should now show up in auto completion

--- init.el
+++ init.el
@@ -47,7 +47,8 @@ This function should only modify configuration layer settings."
      ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     auto-completion
+     (auto-completion :variables
+                        auto-completion-enable-snippets-in-popup t)
      better-defaults
      emacs-lisp
      git
