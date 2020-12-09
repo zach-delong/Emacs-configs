d81b23b441dcfe3656466436ba5ee879e49852ab
zach delong <zach.delong@abrigo.com>
2020-12-09 17:45:47 -0500

    Added python layer

--- init.el
+++ init.el
@@ -32,7 +32,7 @@ This function should only modify configuration layer settings."
 
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
-   '(
+   '(python
      windows-scripts
      sql
      javascript
