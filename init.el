0279f1f99c489d4a591750cfbc55c8094334f874
Zachery DeLong <zacherypd@gmail.com>
2021-08-16 21:13:31 -0400

    Adding zettelkasten layer for random stuff

--- init.el
+++ init.el
@@ -70,6 +70,7 @@ This function should only modify configuration layer settings."
      syntax-checking
      version-control
      (treemacs :variables treemacs-use-git-mode 'simple)
+     zach-zettel
      pocket)
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
