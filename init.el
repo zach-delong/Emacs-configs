f30fc42d216ce73d1625efc6c8615ce8eeef248c
Zachery DeLong <zacherypd@gmail.com>
2022-06-17 10:04:25 -0400

    Checked in init.el autogen changes

--- init.el
+++ init.el
@@ -4,7 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package evil)))
+ '(custom-safe-themes
+   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+ '(package-selected-packages '(use-package evil))
+ '(safe-local-variable-values
+   '((projectile-project-test-cmd . "dotnet test")
+     (projectile-project-compilation-cmd . "dotnet build"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
