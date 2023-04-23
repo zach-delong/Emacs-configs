7caf3e943a8f1ec2dc16cd6416f52aea8e099b8b
Zachery DeLong <zacherypd@gmail.com>
2023-04-23 14:55:43 -0400

    Added safe theme

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
+   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
  '(package-selected-packages '(tide ido-completing-read+ use-package evil))
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "dotnet test")
