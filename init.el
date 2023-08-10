e988db33867e2a048893cbc82bbbd9975a08745f
Zachery DeLong <zacherypd@gmail.com>
2023-08-09 21:07:43 -0400

    Moved my custom changes to a custom file

--- init.el
+++ init.el
@@ -1,19 +1,1 @@
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
- '(package-selected-packages '(tide ido-completing-read+ use-package evil))
- '(safe-local-variable-values
-   '((projectile-project-test-cmd . "dotnet test")
-     (projectile-project-compilation-cmd . "dotnet build"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
