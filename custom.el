1d7b980637aca0a811e058a034896b5a7d70b971
Zachery DeLong <zacherypd@gmail.com>
2023-09-04 14:15:41 -0400

    Checking in

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
+ '(safe-local-variable-values
+   '((projectile-project-test-cmd . "dotnet test")
+     (projectile-project-compilation-cmd . "dotnet build"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
