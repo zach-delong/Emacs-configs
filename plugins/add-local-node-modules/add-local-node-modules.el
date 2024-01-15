30ceadda560a89d3600a78bc1cd37e110fd9ca4a
Zachery DeLong <zacherypd@gmail.com>
2024-01-14 20:52:46 -0500

    Initial skeleton of plugin for adding custom node modules

---
+++ plugins/add-local-node-modules/add-local-node-modules.el
@@ -0,0 +1,9 @@
+(defun add-local-node-modules ()
+  "Attempts to load a node-modules folder into the local path, if it
+can be found. Requires that you have NPM installed and available
+on your path."
+  (interactive)
+
+  (message "Hello, world!"))
+
+(provide 'add-local-node-modules)
