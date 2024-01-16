11de7de170089e8960557eeeba9cc292984662ba
Zachery DeLong <zacherypd@gmail.com>
2024-01-16 18:41:48 -0500

    Revert "Removed bootstrapping process that installs use-package!"
    This reverts commit 70245777725c3266e05c594edee2b2d6531f8b6f.

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(package-selected-packages
-   '(treemacs flycheck markdown-mode magit yasnippet diminish add-node-modules-path prettier-js yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil tide spacemacs-theme python-mode projectile powershell org-ql org-pomodoro org-bullets lsp-mode ido-vertical-mode ido-completing-read+ evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode csharp-mode company auto-package-update adaptive-wrap))
+   '(yasnippet diminish add-node-modules-path prettier-js yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil tide spacemacs-theme python-mode projectile powershell org-ql org-pomodoro org-bullets lsp-mode ido-vertical-mode ido-completing-read+ evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode csharp-mode company auto-package-update adaptive-wrap))
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "dotnet test")
      (projectile-project-compilation-cmd . "dotnet build"))))
