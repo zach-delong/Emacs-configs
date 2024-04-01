71e90d462b934f9230023d3b2f8988633f66a167
Zachery DeLong <zacherypd@gmail.com>
2024-04-01 11:22:54 -0400

    Checking in custom file

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(lsp-log-io t)
  '(package-selected-packages
    '(dashboard treemacs flycheck markdown-mode magit yasnippet diminish add-node-modules-path prettier-js yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column use-package undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil tide spacemacs-theme python-mode projectile powershell org-ql org-pomodoro org-bullets lsp-mode ido-vertical-mode ido-completing-read+ evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode csharp-mode company auto-package-update adaptive-wrap))
