60e4677fac67bfa494c76ab29565ffb918d6e917
Zachery DeLong <zacherypd@gmail.com>
2024-08-08 22:01:39 -0400

    Adding solarized theme and defaulted to dark

--- init.el
+++ init.el
@@ -1,2 +1,15 @@
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(solarized-theme yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil spacemacs-theme python-mode prettier-js powershell org-ql org-pomodoro org-bullets lsp-treemacs flycheck evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode diminish dashboard company auto-package-update adaptive-wrap)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
