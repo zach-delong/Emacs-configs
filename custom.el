c8ab8b761eba6a530837b9045ef2525d5b1741cb
Zachery DeLong <zacherypd@gmail.com>
2024-12-19 10:46:42 -0500

    Fixing random isues

--- custom.el
+++ custom.el
@@ -13,9 +13,4 @@
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "dotnet test")
      (projectile-project-compilation-cmd . "dotnet build"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:foreground "#839496" :background "#002b36")))))
+
