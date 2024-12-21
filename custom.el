9b6737addd51c63afe8c591cc959626083ac0c87
Zachery DeLong <zacherypd@gmail.com>
2024-12-21 15:01:21 -0500

    added vim-tab-bar

--- custom.el
+++ custom.el
@@ -9,8 +9,14 @@
  '(lsp-log-io t)
  '(max-lisp-eval-depth 16000)
  '(package-selected-packages
-   '(solarized-theme yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil spacemacs-theme python-mode prettier-js powershell org-ql org-pomodoro org-bullets lsp-treemacs flycheck evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode diminish dashboard company auto-package-update adaptive-wrap))
+   '(vim-tab-bar solarized-theme yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil spacemacs-theme python-mode prettier-js powershell org-ql org-pomodoro org-bullets lsp-treemacs flycheck evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode diminish dashboard company auto-package-update adaptive-wrap))
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "dotnet test")
      (projectile-project-compilation-cmd . "dotnet build"))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
