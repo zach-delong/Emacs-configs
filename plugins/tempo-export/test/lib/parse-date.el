03e6c7bc59d3e86e64c0b4dbcfca98e8136fb638
Zachery DeLong <zacherypd@gmail.com>
2025-09-14 20:43:59 -0400

    Added some more tests

--- plugins/tempo-export/test/lib/parse-date.el
+++ plugins/tempo-export/test/lib/parse-date.el
@@ -8,4 +8,10 @@
   (should (= (alist-get :start (get-start-and-end 100 1234)) 100))
   (should (= (alist-get :end (get-start-and-end 100 1234)) 1234)))
 
+(ert-deftest a-gt-b ()
+  (should (= (alist-get :start (get-start-and-end 2 1)) 1))
+  (should (= (alist-get :end (get-start-and-end 2 1))))
+  (should (= (alist-get :start (get-start-and-end 4 25)) 4))
+  (should (= (alist-get :end (get-start-and-end 4 25)) 25)))
+
 (provide 'parse-date-table-tests)
