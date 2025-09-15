2293924b21cd70caa888a98af6de2926a220b304
Zachery DeLong <zacherypd@gmail.com>
2025-09-14 21:44:35 -0400

    Added is-date-p
    I should be able to use this to identify what lines are dates

--- plugins/tempo-export/src/lib.el
+++ plugins/tempo-export/src/lib.el
@@ -5,4 +5,14 @@
          (end (if a-is-first b a)))
     (list (cons :start start) (cons :end end))))
 
+(defun is-date-p (date)
+  "Is the provided item a date?"
+  (if (or
+       (not (stringp date))
+       (equal date ""))
+      nil
+    (string-match "[\[]?[0-9]+-[0-9]+-[0-9]+ [A-Za-z]+[\]]?" date)))
+
 (provide 'parse-date-table)
+
+(string-match "[\[]?[A-Za-z]+\]" "[asdf]")
---
+++ plugins/tempo-export/test/lib/is-date-p.el
@@ -0,0 +1,13 @@
+(load-file (expand-file-name "plugins/tempo-export/src/lib.el" user-emacs-directory))
+
+(ert-deftest is-date-should-be-nill-for-non-strings ()
+  (should (equal (is-date-p 1) nil))
+  (should (equal (is-date-p 'asdf) nil)))
+
+(ert-deftest is-date-should-find-dates ()
+  (should (not (equal (is-date-p "[2025-08-25 Mon]") nil)))
+  (should (not (equal (is-date-p "2025-08-25 Mon") nil))))
+
+(ert-deftest is-date-nonstring-date ()
+  (should (equal (is-date-p "") nil))
+  (should (equal (is-date-p "asdf") nil)))
