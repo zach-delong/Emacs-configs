53ba91bb9556a1a25f204c43a437581897898c77
Zachery DeLong <zacherypd@gmail.com>
2025-09-29 20:41:07 -0400

    Checked in test over multiple tables

--- plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
+++ plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
@@ -7,3 +7,23 @@
       (set-mark (point-min))
       (goto-char (point-max))
       (should (equal "Daily report: [2025-08-25 Mon]\n" (car (tempo-build-table-list)))))))
+
+(ert-deftest process-org-timesheet-buffer-multiple-tables ()
+  "Notice that the list is in the reverse order"
+  (save-excursion
+    (with-temp-buffer
+      (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
+	      "\n"
+	      "Daily report: [2025-08-25 Mon]\n" "| File | Headline         | Time   "
+	      "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
+	      "\n"
+	      "Daily report: [2025-08-26 Tue]\n" "| File | Headline         | Time   ")
+      (set-mark (point-min))
+      (goto-char (point-max))
+      (should
+       (equal
+	"Daily report: [2025-08-26 Tue]\n"
+	(car (tempo-build-table-list))))
+      (should
+       (equal "Daily report: [2025-08-25 Mon]\n"
+	      (nth 1 (tempo-build-table-list)))))))
