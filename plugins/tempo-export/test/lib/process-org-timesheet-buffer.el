43b0cfc3cee46d2dda776ece2b76b36b569b4bb3
Zachery DeLong <zacherypd@gmail.com>
2025-10-11 21:10:44 -0400

    Fixed tests to use a new method

--- plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
+++ plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
@@ -1,32 +1,37 @@
 (load-file (expand-file-name "plugins/tempo-export/src/lib.el" user-emacs-directory))
 
+(defmacro arrange-test (&rest body)
+  "A macro that accepts emacs lisp code, and evaluates it in a temporary buffer, saving excursion before it goes to preserve the location."
+  `(progn
+     (save-excursion
+       (with-temp-buffer
+	 ,@body))))
+
 (ert-deftest process-org-timesheet-buffer-should-work ()
-  (save-excursion
-    (with-temp-buffer
-      (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n" "\n" "Daily report: [2025-08-25 Mon]\n" "| File | Headline         | Time   ")
-      (set-mark (point-min))
-      (goto-char (point-max))
-      (should (equal "2025-08-25" (car (tempo-build-table-list)))))))
+  (arrange-test
+   (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n" "\n" "Daily report: [2025-08-25 Mon]\n" "| File | Headline         | Time   ")
+   (set-mark (point-min))
+   (goto-char (point-max))
+   (should (equal "2025-08-25" (car (tempo-build-table-list))))))
 
 (ert-deftest process-org-timesheet-buffer-multiple-tables ()
   "Notice that the list is in the reverse order"
-  (save-excursion
-    (with-temp-buffer
-      (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
-	      "\n"
-	      "Daily report: [2025-08-25 Mon]\n" "| File | Headline         | Time   "
-	      "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
-	      "\n"
-	      "Daily report: [2025-08-26 Tue]\n" "| File | Headline         | Time   ")
-      (set-mark (point-min))
-      (goto-char (point-max))
-      (should
-       (equal
-	"2025-08-25"
-	(car (tempo-build-table-list))))
-      (should
-       (equal "2025-08-26"
-	      (nth 1 (tempo-build-table-list)))))))
+  (arrange-test
+   (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
+	   "\n"
+	   "Daily report: [2025-08-25 Mon]\n" "| File | Headline         | Time   "
+	   "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
+	   "\n"
+	   "Daily report: [2025-08-26 Tue]\n" "| File | Headline         | Time   ")
+   (set-mark (point-min))
+   (goto-char (point-max))
+   (should
+    (equal
+     "2025-08-25"
+     (car (tempo-build-table-list))))
+   (should
+    (equal "2025-08-26"
+	   (nth 1 (tempo-build-table-list))))))
 
 (ert-deftest process-complex-org-timesheet-buffer ()
   "here's a nasty example"
