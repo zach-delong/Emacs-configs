343fac1d65236b3d7ac370d82cc16f61a2152e04
Zach DeLong <zpdelong@alumni.ncsu.edu>
2026-02-24 21:34:12 -0500

    Why on earth did I format those tests that way?

--- plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
+++ plugins/tempo-export/test/lib/process-org-timesheet-buffer.el
@@ -8,15 +8,14 @@
 	 ,@body))))
 
 (ert-deftest process-org-timesheet-buffer-should-work ()
-  (let ((strings (concat "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
-			 "\n"
-			 "Daily report: [2025-08-25 Mon]\n"
-			 "| File | Headline         | Time  ")))
-    (arrange-test
-     (insert strings)
-     (set-mark (point-min))
-     (goto-char (point-max))
-     (should (equal "2025-08-25" (car (tempo-build-table-list)))))))
+  (arrange-test
+   (insert "#+BEGIN: clocktable :block thisweek :scope agenda :maxlevel 10 :step day\n"
+	   "\n"
+	   "Daily report: [2025-08-25 Mon]\n"
+	   "| File | Headline         | Time  ")
+   (set-mark (point-min))
+   (goto-char (point-max))
+   (should (equal "2025-08-25" (car (tempo-build-table-list))))))
 
 (ert-deftest process-org-timesheet-buffer-multiple-tables ()
   "Notice that the list is in the reverse order"
