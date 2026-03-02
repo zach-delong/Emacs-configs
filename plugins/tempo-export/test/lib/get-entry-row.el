dd115cb797124ff692fffb10f4853d7e6cc37cbb
Zach DeLong <zpdelong@alumni.ncsu.edu>
2026-03-01 20:09:27 -0500

    Reformatted tests and fixed titles

--- plugins/tempo-export/test/lib/get-entry-row.el
+++ plugins/tempo-export/test/lib/get-entry-row.el
@@ -1,12 +1,16 @@
 (load-file (expand-file-name "plugins/tempo-export/src/lib.el" user-emacs-directory))
 
-(ert-deftest testing ()
-  (should (equal (get-entry-row "| stuff | things |") '("stuff" "things"))))
+(ert-deftest get-entry-row/simple-row-should-flag ()
+  (should (equal
+	   (get-entry-row "| stuff | things |")
+	   '("stuff" "things"))))
 
-(ert-deftest testing2 ()
-  (should (equal (get-entry-row "|                  | \_          more stuff |        |      |      |      |      | 0:03 |")
-				'("\_          more stuff" "0:03"))))
+(ert-deftest get-entry-row/long-row-should-flag ()
+  (should (equal
+	   (get-entry-row "|                  | \_          more stuff |        |      |      |      |      | 0:03 |")
+	   '("\_          more stuff" "0:03"))))
 
-(ert-deftest testing3 ()
-  (should (equal (get-entry-row "|                  | \_          more stuff |        |      |      |      | 0:03 |      |")
-				nil)))
+(ert-deftest get-entry-row/non-entry-row-should-nil ()
+  (should (equal
+	   (get-entry-row "|                  | \_          more stuff |        |      |      |      | 0:03 |      |")
+	   nil)))
