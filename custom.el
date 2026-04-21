e10f124ccdbe95b0b4916ebf359dd68439dd4d11
Zach DeLong <zpdelong@alumni.ncsu.edu>
2026-04-21 13:15:58 -0400

    Tweaking init
    moved custom variables to custom file

--- custom.el
+++ custom.el
@@ -4,12 +4,27 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
+   '("01f347a923dd21661412d4c5a7c7655bf17fb311b57ddbdbd6fce87bd7e58de6"
+     "9af2b1c0728d278281d87dc91ead7f5d9f2287b1ed66ec8941e97ab7a6ab73c0"
+     "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf"
+     "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088"
+     default))
  '(eglot-connect-timeout 120)
  '(lsp-log-io t)
  '(max-lisp-eval-depth 16000)
  '(package-selected-packages
-   '(vim-tab-bar solarized-theme yasnippet-snippets yaml-mode which-key web-mode vue-mode visual-fill-column undo-tree typescript-mode treemacs-tab-bar treemacs-magit treemacs-icons-dired treemacs-evil spacemacs-theme python-mode prettier-js powershell org-ql org-pomodoro org-bullets lsp-treemacs flycheck evil-surround evil-org evil-collection evil-cleverparens editorconfig dockerfile-mode diminish dashboard company auto-package-update adaptive-wrap))
+   '(solarized-theme yasnippet-snippets yaml-mode which-key web-mode
+		     vue-mode visual-fill-column undo-tree
+		     typescript-mode treemacs-tab-bar treemacs-magit
+		     treemacs-icons-dired treemacs-evil
+		     spacemacs-theme python-mode prettier-js
+		     powershell org-ql org-pomodoro org-bullets
+		     lsp-treemacs flycheck evil-surround evil-org
+		     evil-collection evil-cleverparens editorconfig
+		     dockerfile-mode diminish dashboard company
+		     auto-package-update adaptive-wrap))
  '(safe-local-variable-values
    '((projectile-project-test-cmd . "dotnet test")
      (projectile-project-compilation-cmd . "dotnet build"))))
@@ -20,3 +35,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(put 'narrow-to-region 'disabled nil)
